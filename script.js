36bffe484f2ee8991a30b8df8f1d231561416f1e
Veronica Williams <veronica.robin.williams@gmail.com>
2016-04-05 00:15:08 -0400

    Javascript connects to HTML

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 
 
-
-
-function makeMoreExciting(string) {
-  return string + '!!!!'
+function Nice()
+{
+    var text_box = document.getElementById('typey_box');
+    var results_box = document.getElementById('output');
+    var text = text_box.value;
+    var message = "You are a nice person," + text;
+    results_box.innerHTML = message;
 }
+
+//function makeMoreExciting(string) {
+//  return string + '!!!!'
+//}
